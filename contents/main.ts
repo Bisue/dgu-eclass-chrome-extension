be8162c5a64a967eec5297d634b870f9dd6bb079
Chanhyuk Byeon <bdu00chch@gmail.com>
2023-05-02 03:58:10 +0900

    eclass 메인 enable 메세지

---
+++ contents/main.ts
@@ -0,0 +1,28 @@
+import type { PlasmoCSConfig } from "plasmo"
+
+export const config: PlasmoCSConfig = {
+  matches: ["https://eclass.dongguk.edu/Main.do*"],
+  css: ["main.css"]
+}
+
+window.addEventListener("load", () => {
+  // setTimeout(() => {
+  console.log("동국대 이클래스 헬퍼 - CSE 18th BCH")
+
+  const newHeader = document.createElement("div")
+  newHeader.textContent = "동국대 이클래스 헬퍼 적용 완료"
+  newHeader.className = "bisue-header"
+
+  const header = document.querySelector("#header") as HTMLElement
+  // header.style.transition = "background 0.5s"
+  // header.style.background = "#bae6fd"
+  // header.style.color = "#171717"
+
+  header.insertBefore(newHeader, header.firstChild)
+
+  // const dummy = document.createElement("div")
+  // dummy.style.height = "80px"
+  // const container = document.querySelector("#container") as HTMLElement
+  // container.insertBefore(dummy, container.firstChild)
+  // }, 1000)
+})
