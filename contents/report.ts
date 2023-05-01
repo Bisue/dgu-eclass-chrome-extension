a850d83834be781867bf74f348f7d283aaca9888
Chanhyuk Byeon <bdu00chch@gmail.com>
2023-05-02 03:59:19 +0900

    과제 제출률 조회 (migrate from local js)

---
+++ contents/report.ts
@@ -0,0 +1,73 @@
+import type { PlasmoCSConfig } from "plasmo"
+
+export const config: PlasmoCSConfig = {
+  matches: ["https://eclass.dongguk.edu/Report.do"],
+  css: ["report.css"]
+}
+
+function getSubmitInfo(document: HTMLDocument) {
+  const infoTable = document.querySelectorAll(
+    "#listBox .boardListWrite tbody"
+  )[1]
+  if (!infoTable) {
+    return false
+  }
+
+  const tempEl = document.createElement("div")
+  tempEl.innerHTML = (
+    [...infoTable.childNodes].find((el) => el.nodeType == 8) as Comment
+  ).data.trim()
+  document.body.appendChild(tempEl)
+
+  const dataRaw = tempEl.innerText
+  console.log(dataRaw)
+  const reportInfoDTORaw = dataRaw.split("ReportInfoDTO")[1]
+
+  const submitCnt = Number.parseInt(
+    reportInfoDTORaw.match(/submitCnt=([0-9]+),/)[1]
+  )
+  const lateSubmitCnt = Number.parseInt(
+    reportInfoDTORaw.match(/lateSubmitCnt=([0-9]+),/)[1]
+  )
+  const studentCnt = Number.parseInt(
+    reportInfoDTORaw.match(/studentCnt=([0-9]+),/)[1]
+  )
+
+  document.body.removeChild(tempEl)
+
+  return {
+    submitCnt: submitCnt,
+    studentCnt: studentCnt,
+    lateSubmitCnt: lateSubmitCnt,
+    percent: ((submitCnt + lateSubmitCnt) / studentCnt) * 100
+  }
+}
+
+window.addEventListener("load", () => {
+  let submitInfo: ReturnType<typeof getSubmitInfo>
+
+  const frame = document.querySelector("frame[name='main']") as HTMLFrameElement
+  if (frame) {
+    submitInfo = getSubmitInfo(frame.contentDocument)
+  } else {
+    submitInfo = getSubmitInfo(document)
+  }
+
+  if (submitInfo === false) {
+    return
+  }
+
+  const outputTbody = document.querySelectorAll(
+    "#listBox .boardListWrite tbody"
+  )[0]
+
+  const outputRow = document.createElement("tr")
+  outputRow.className = "odd-row"
+  outputRow.innerHTML = `
+  <th class="head first">제출률(by Extended Eclass)</th>
+  <td class="last bisue-submit-tr">${submitInfo.submitCnt}(+${
+    submitInfo.lateSubmitCnt
+  }) / ${submitInfo.studentCnt} (${submitInfo.percent.toFixed(2)}%)</td>
+  `
+  outputTbody.appendChild(outputRow)
+})
