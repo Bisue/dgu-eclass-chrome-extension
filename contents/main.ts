40f7166f072b3c25592c2c0842623cae87a95a32
Chanhyuk Byeon <bdu00chch@gmail.com>
2024-03-30 18:00:51 +0900

    feat: 활성화 여부 표시

--- contents/main.ts
+++ contents/main.ts
@@ -1,28 +1,17 @@
 import type { PlasmoCSConfig } from 'plasmo';
 
 export const config: PlasmoCSConfig = {
-  matches: ['https://eclass.dongguk.edu/Main.do*'],
+  matches: ['https://eclass.dongguk.edu/*'],
   css: ['main.css'],
 };
 
+/**
+ * Injection
+ */
 window.addEventListener('load', () => {
-  // setTimeout(() => {
-  console.log('동국대 Extended Eclass - CSE 18th BCH');
+  const captionEl = document.createElement('div');
+  captionEl.textContent = '이클래스 Extension이 활성화되었습니다!';
+  captionEl.className = 'bisue-caption';
 
-  // const newHeader = document.createElement("div")
-  // newHeader.textContent = "동국대 Extended Eclass - Enabled"
-  // newHeader.className = "bisue-header"
-
-  // const header = document.querySelector("#header") as HTMLElement
-  // header.style.transition = "background 0.5s"
-  // header.style.background = "#bae6fd"
-  // header.style.color = "#171717"
-
-  // header.insertBefore(newHeader, header.firstChild)
-
-  // const dummy = document.createElement("div")
-  // dummy.style.height = "80px"
-  // const container = document.querySelector("#container") as HTMLElement
-  // container.insertBefore(dummy, container.firstChild)
-  // }, 1000)
+  document.body.appendChild(captionEl);
 });
